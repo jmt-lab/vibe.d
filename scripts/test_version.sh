c5db65850388aa12fb015c93b51f6a4d9b8250c9
Sönke Ludwig <sludwig@outerproduct.org>
2021-09-14 10:45:12 +0200

    Remove dysfunctional vibe:core version check.

--- scripts/test_version.sh
+++ scripts/test_version.sh
@@ -4,11 +4,10 @@ set -xueo pipefail
 
 # test version strings
 ver_git=$(git describe --abbrev=0 --tags)
-ver_vibe=$(sed -n 's|.*vibeVersionString\s*= "\(.*\)";$|\1|p' core/vibe/core/core.d)
 ver_meson_base=$(sed -n 's|\s*version\s*: '"'"'\(.*\)'"'"'$|\1|p' meson.build)
 ver_meson_suffix=$(sed -n 's|project_version_suffix\s*= '"'"'~\([a-z]*\)\([0-9]*\)'"'"'$|-\1.\2|p' meson.build)
 
-if [ "${ver_git}" != v"$ver_vibe" -o "${ver_git}" != "v$ver_meson_base$ver_meson_suffix" ]; then
+if [ "${ver_git}" != "v$ver_meson_base$ver_meson_suffix" ]; then
     echo "Mismatch between versions."
     echo "	git: '$ver_git'"
     echo "	vibeVersionString: '$ver_vibe'"
